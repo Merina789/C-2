ca315b4322abded98c296ba3e70b585816fba681
Xixi027 <252541897@qq.com>
2024-01-18 16:49:08 +0800

    新代码

---
+++ 1-1.第一个C语言程序/1-1.Helloworld/1.Helloworld/Helloworld.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+  //包含一个叫stdio.h的文件
+  //std 标准 standard input output 
+  //# 一种指令形式
+
+#include<stdio.h>
+
+int main()  //程序的入口-main函数有且仅有一个
+{
+	  //从这里开始
+	  //在屏幕上输出hello world
+	  //函数 print funcion   printf- 打印函数
+	  //库函数C语言本身提供给我们使用的函数
+      //别人的东西要打招呼
+	printf("Hello world\n");
+
+	return 0;
+}
+/*
+  //int 整型的意思
+  //main前面的int表示main函数调用返回一个整型值
+	int main
+	{
+
+	  return 0; //返回0
+	}
+*/
+
+/*
+  2024年1月8日 周一
+  功能：对程序的一些基本解释
+*/
+
+/*
+输出结果
+—————————————————————————————
+Hello world
+—————————————————————————————
+   总结：记住每个解释的含义
+         printf("\n") 的格式
+ */
